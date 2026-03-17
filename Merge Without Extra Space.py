0282f98f0981bd977b04ce21758d3a3294eeeea2
Himanshpapnai <152257435+Himanshpapnai@users.noreply.github.com>
2026-03-17 19:35:48 +0530

    Create Merge Without Extra Space.py

---
+++ Merge Without Extra Space.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeArrays(self, a, b):
+        n, m = len(a), len(b)
+        i = n - 1
+        j = 0
+        while i >= 0 and j < m:
+            if a[i] > b[j]:
+                a[i], b[j] = b[j], a[i]
+                i -= 1
+                j += 1
+            else:
+                break
+        a.sort()
+        b.sort()
